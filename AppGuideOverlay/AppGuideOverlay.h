45b61df0bb60665cee060518e3134a77ff10ae81
Christian Tietze <christian.tietze@gmail.com>
2018-01-27 14:56:57 +0100

    Initial Commit

---
+++ AppGuideOverlay/AppGuideOverlay.h
@@ -0,0 +1,19 @@
+//
+//  AppGuideOverlay.h
+//  AppGuideOverlay
+//
+//  Created by Christian Tietze on 27.01.18.
+//  Copyright © 2018 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AppGuideOverlay.
+FOUNDATION_EXPORT double AppGuideOverlayVersionNumber;
+
+//! Project version string for AppGuideOverlay.
+FOUNDATION_EXPORT const unsigned char AppGuideOverlayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppGuideOverlay/PublicHeader.h>
+
+
